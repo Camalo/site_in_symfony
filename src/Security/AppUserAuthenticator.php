a0f725c3741b5a8d1feea9a2a2305e42000beda3
camal <camalovalsu29@gmail.com>
2020-11-06 20:59:02 +0300

    little updating of login

--- src/Security/AppUserAuthenticator.php
+++ src/Security/AppUserAuthenticator.php
@@ -8,6 +8,8 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationChecker;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 use Symfony\Component\Security\Core\Exception\InvalidCsrfTokenException;
@@ -30,13 +32,17 @@ class AppUserAuthenticator extends AbstractFormLoginAuthenticator implements Pas
     private $urlGenerator;
     private $csrfTokenManager;
     private $passwordEncoder;
+    private $userRole;
+    private $authorizationChecker;
 
-    public function __construct(EntityManagerInterface $entityManager, UrlGeneratorInterface $urlGenerator, CsrfTokenManagerInterface $csrfTokenManager, UserPasswordEncoderInterface $passwordEncoder)
+
+    public function __construct(EntityManagerInterface $entityManager, UrlGeneratorInterface $urlGenerator, CsrfTokenManagerInterface $csrfTokenManager, UserPasswordEncoderInterface $passwordEncoder,AuthorizationCheckerInterface $authorizationChecker)
     {
         $this->entityManager = $entityManager;
         $this->urlGenerator = $urlGenerator;
         $this->csrfTokenManager = $csrfTokenManager;
         $this->passwordEncoder = $passwordEncoder;
+        $this->authorizationChecker=$authorizationChecker;
     }
 
     public function supports(Request $request)
@@ -63,11 +69,13 @@ class AppUserAuthenticator extends AbstractFormLoginAuthenticator implements Pas
     public function getUser($credentials, UserProviderInterface $userProvider)
     {
         $token = new CsrfToken('authenticate', $credentials['csrf_token']);
+
         if (!$this->csrfTokenManager->isTokenValid($token)) {
             throw new InvalidCsrfTokenException();
         }
 
         $user = $this->entityManager->getRepository(User::class)->findOneBy(['email' => $credentials['email']]);
+
 
         if (!$user) {
             // fail authentication with a custom error
@@ -76,6 +84,8 @@ class AppUserAuthenticator extends AbstractFormLoginAuthenticator implements Pas
 
         return $user;
     }
+
+
 
     public function checkCredentials($credentials, UserInterface $user)
     {
@@ -92,8 +102,14 @@ class AppUserAuthenticator extends AbstractFormLoginAuthenticator implements Pas
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $providerKey)
     {
+        $targetP = $this->urlGenerator->generate('manager');
+
         if ($targetPath = $this->getTargetPath($request->getSession(), $providerKey)) {
             return new RedirectResponse($targetPath);
+        }
+
+        if ($this->authorizationChecker->isGranted('ROLE_ADMIN')) {
+            $targetP = $this->urlGenerator->generate('admin');
         }
 
         return new RedirectResponse($this->urlGenerator->generate('admin'));
