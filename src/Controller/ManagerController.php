ab2f9b7b83cbb2cd4ffbd8e076a3c9fa05609632
Camalo <60464321+Camalo@users.noreply.github.com>
2020-11-19 18:19:52 +0300

    Update ManagerController.php

--- src/Controller/ManagerController.php
+++ src/Controller/ManagerController.php
@@ -58,7 +58,7 @@ class ManagerController extends AbstractController
 
 
     /**
-     * @Route("/public/manager/new", name="new_book")
+     * @Route("public/manager/new", name="new_book")
      */
     public function newBook(Request $request):Response
     {
@@ -83,7 +83,7 @@ class ManagerController extends AbstractController
         ]);
     }
     /**
-     * @Route("/public/manager/update/{id}", name="update_book")
+     * @Route("public/manager/update/{id}", name="update_book")
      */
     public function updateBook(Request $request, $id)//:Response
     {
@@ -111,7 +111,7 @@ class ManagerController extends AbstractController
 
 
     /**
-     * @Route("/public/manager/delete/{id}", name="delete_book")
+     * @Route("public/manager/delete/{id}", name="delete_book")
      */
     public function deleteBook($id)
     {
@@ -160,7 +160,7 @@ class ManagerController extends AbstractController
     }
 
     /**
-     * @Route("/public/manager/add_to/{id}", name="add_to")
+     * @Route("public/manager/add_to/{id}", name="add_to")
      */
     public function addBook(Request $request,$id)
     {
@@ -187,7 +187,7 @@ class ManagerController extends AbstractController
         ]);
     }
     /**
-     * @Route("/public/manager/delete_category/{id}", name="delete_category")
+     * @Route("public/manager/delete_category/{id}", name="delete_category")
      */
     public function deleteCategory($id)
     {
@@ -202,7 +202,7 @@ class ManagerController extends AbstractController
     }
 
     /**
-     * @Route("/public/manager/delete_from/{catid}/{bookid}", name="delete_from")
+     * @Route("public/manager/delete_from/{catid}/{bookid}", name="delete_from")
      */
     public function deleteFrom($catid,$bookid)
     {
