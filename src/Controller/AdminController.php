e48592c9aacd05913ac4d424a44c669a25528d58
Camalo <60464321+Camalo@users.noreply.github.com>
2020-11-20 14:18:05 +0300

    change route ('/admin')

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -18,7 +18,7 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 class AdminController extends AbstractController
 {
     /**
-     * @Route("public/admin", name="admin")
+     * @Route("/admin", name="admin")
      */
     public function index(AuthorizationCheckerInterface $authChecker): Response
     {
